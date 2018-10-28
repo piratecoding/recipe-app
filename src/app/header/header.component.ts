1c248c2e54ebbfcb9b23ad046ef8d84aae08d1d3
rails -v <amaratc@gmail.com>
2018-10-28 21:23:32 +0100

    Init commit 2

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: '/header.component.html'
+})
+export class HeaderComponent {
+  @Output() featureSelect = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelect.emit(feature);
+  }
+  
+}
